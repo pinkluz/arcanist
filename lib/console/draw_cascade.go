771fb43f65e50e4ac7e7b30c3394b8318ca9179d
Michael Schuett <mschuett@ext.uber.com>
2021-06-26 19:22:45 -0500

    Give it some color

--- lib/console/draw_cascade.go
+++ lib/console/draw_cascade.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/pinkluz/arcanist/lib/git"
 )
 
@@ -19,14 +20,18 @@ func DrawCascade(r git.RecursiveRebaseStatus, opts *DrawOpts) string {
 			fmt.Sprintf("We couldn't rebase all your branches %d of %d failed",
 				len(r.FailedBraches),
 				len(r.SuccessfulBranches)+len(r.FailedBraches)))
+		ret = append(ret, "")
 	} else {
 		ret = append(ret,
 			fmt.Sprintf("All %d branches were rebased", len(r.SuccessfulBranches)))
 	}
 
 	for _, branch := range r.FailedBraches {
+		bad := lipgloss.NewStyle().
+			Foreground(lipgloss.AdaptiveColor{Light: "#FA4659", Dark: "#FA4659"})
+
 		ret = append(ret,
-			fmt.Sprintf("%s "+cross_mark, branch))
+			fmt.Sprintf("%s "+bad.Render(cross_mark), branch))
 	}
 
 	return strings.Join(ret, "\n")
