5232f9fd27d7f60507667cc2f4ff421a47392639
Michael Schuett <mschuett@ext.uber.com>
2021-06-26 19:07:36 -0500

    Cleanup status bar and make it smooth

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/charmbracelet/lipgloss v0.3.0
-	github.com/cheggaaa/pb v1.0.29
+	github.com/cheggaaa/pb/v3 v3.0.8
 	github.com/fatih/color v1.12.0
 	github.com/go-git/go-git/v5 v5.4.2
 	github.com/spf13/cobra v1.1.3
--- lib/git/rebase.go
+++ lib/git/rebase.go
@@ -3,7 +3,7 @@ package git
 import (
 	"fmt"
 
-	"github.com/cheggaaa/pb"
+	pb "github.com/cheggaaa/pb/v3"
 	gogit "github.com/go-git/go-git/v5"
 )
 
@@ -34,6 +34,7 @@ func RecursiveRebase(repo *gogit.Repository) (*RecursiveRebaseStatus, error) {
 	}
 
 	loadingBar := pb.StartNew(val.CountDownstreams())
+	loadingBar.Set(pb.CleanOnFinish, true)
 	statusMap := map[string][]string{}
 	for _, node := range val.Downstream {
 		failed, success, err := rebase(node, []string{}, []string{}, loadingBar)
@@ -59,9 +60,10 @@ func RecursiveRebase(repo *gogit.Repository) (*RecursiveRebaseStatus, error) {
 }
 
 func rebase(n *BranchNode, failed []string, success []string, l *pb.ProgressBar) ([]string, []string, error) {
+	l.Increment()
+
 	err := CheckoutRaw(n.Name)
 	if err != nil {
-		l.Increment()
 		return append(failed, n.Name), success, err
 	}
 
@@ -70,18 +72,15 @@ func rebase(n *BranchNode, failed []string, success []string, l *pb.ProgressBar)
 		// Before we fail try to recover from the interactive rebase.
 		err := AbortRebase()
 		if err != nil {
-			l.Increment()
 			return append(failed, n.Name), success, err
 		}
 
 		// We keep going but we have no reason to try and rebase the downstream branches
 		// as they will all fail as well.
-		l.Increment()
 		return append(failed, n.Name), success, err
 	}
 
 	for _, node := range n.Downstream {
-		l.Increment()
 		f, s, err := rebase(node, failed, success, l)
 		if err != nil {
 			return append(failed, f...), append(success, s...), err
@@ -91,6 +90,5 @@ func rebase(n *BranchNode, failed []string, success []string, l *pb.ProgressBar)
 		success = append(success, s...)
 	}
 
-	l.Increment()
 	return failed, append(success, n.Name), err
 }
