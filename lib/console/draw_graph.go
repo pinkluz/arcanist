22b2e504ec33a2d3f069716ae7699a9b0b2f1d59
Michael Schuett <michaeljs1990@gmail.com>
2022-01-28 23:49:29 -0600

    Better message

--- lib/console/draw_graph.go
+++ lib/console/draw_graph.go
@@ -39,7 +39,7 @@ func DrawGraph(bnw git.BranchNodeWrapper, opts *DrawOpts) string {
 
 	// Not much to do here
 	if bnw.IsEmpty() {
-		return "Nothing found to render"
+		return "No branches found"
 	}
 
 	// This could be part of the recursive drawLine call but is pulled out to
